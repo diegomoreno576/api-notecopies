998a7f40100a59dd45688293520be5a90878250e
diego576 <diegomoreno576@gmail.com>
2022-08-07 19:46:02 +0200

    deploy problem

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,12 +1,13 @@
 class JsonWebToken
     class << self
+      SECRET_KEY = 'ENV[“API_KEY”]'
       def encode(payload, exp = 24.hours.from_now)
         payload[:exp] = exp.to_i
-        JWT.encode(payload, Rails.application.secrets.secret_key_base)
+        JWT.encode(payload, SECRET_KEY)
       end
    
       def decode(jwt)
-        body = JWT.decode(jwt, Rails.application.secrets.secret_key_base)[0]
+        body = JWT.decode(jwt, SECRET_KEY)[0]
         HashWithIndifferentAccess.new body
       rescue
         nil
