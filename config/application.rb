4a6e96620bc1369b9ae5c2dea137f24a0f296479
diego576 <diegomoreno576@gmail.com>
2022-08-07 22:14:35 +0200

    fix deploy

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module Notecopiesapi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.autoload_paths << "#{Rails.root}/lib"
-    config.eager_load = true
-    config.eager_load_paths += %W(#{config.root}/app/lib)
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -25,7 +23,6 @@ module Notecopiesapi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-
     config.api_only = true
   end
 end
