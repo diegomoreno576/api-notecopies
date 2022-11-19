e2c21d67ca8312e9ae3bd4bba8c22d17e7d8a098
diego576 <diegomoreno576@gmail.com>
2022-11-19 17:13:01 +0100

    nueva api

--- app/controllers/api/v1/company_controller.rb
+++ app/controllers/api/v1/company_controller.rb
@@ -1,20 +1,4 @@
-class Api::V1::BlogController < ApplicationController
-    before_action :authenticate_user
-  
-    def index
-        user = User.find_by(username: auth_params[:username])
-        render json: {
-          company: UserSerializer.new(user),
-        }
-      end
-  
-    private
-  
-  
-    def authenticate_user
-      decoded_token = decode(request.headers['token'])
-      @user = User.find(decoded_token["user_id"])
-      render json: { message: 'Un-Authenticated Request', authenticated: false } unless @user
-    end
+class Api::V1::CompanyController < ApplicationController
+   
   end
   
