3f82b50943e8cbc789ca506467b89feddc618c71
diego576 <diegomoreno576@gmail.com>
2022-11-19 17:01:43 +0100

    nueva api

--- app/serializers/company_serializer.rb
+++
@@ -1,8 +0,0 @@
-class CompannySerializer
-    include FastJsonapi::ObjectSerializer
-    attributes :id, :code, :name
-    
-    attribute :companies do |user|   
-        UserSerializer.new(user.companies)
-      end
-  end
