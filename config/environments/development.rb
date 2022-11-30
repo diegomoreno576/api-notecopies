81ac8ed3b4371961f3e52579d63f3227a1b2dfa9
diego576 <diegomoreno576@gmail.com>
2022-11-30 22:56:11 +0100

    deploy to server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
 
   # Enable server timing
   config.server_timing = true
+  config.hosts << "prueba.notecopies.net"
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -63,3 +64,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
+
+
